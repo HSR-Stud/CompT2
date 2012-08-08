3c9dc298fd204a5effc8384dc4037f444c8344fb
Raphael Nestler <rnestler@hsr.ch>
2012-08-08 16:48:54 +0200

    Kommentar marker in listings

--- listings/SwitchCase.asm
+++ listings/SwitchCase.asm
@@ -4,31 +4,31 @@
 * I=N ActionN
 * I>N Exception
 
-  CMP.B    #N,I         Test for I out of Range
-  BGT      EXCEPTION    IF I>N THEN exception
-  MOVE.W   I,D0         Pick up the Value of I
-  MULU     #4,D0        Eeach Address is a Longword
-  LEA      Table,A0     A0 points to table 
-                        of addresses
-  LEA      (A0,D0),A0   A0 now points to case I 
-                        in Table
-  MOVEA.L  (A0),A0      A0 contains address of
-                        case I handler
-  JMP      (A0)         Executes case I handler
+  CMP.B    #N,I       * Test for I out of Range
+  BGT      EXCEPTION  * IF I>N THEN exception
+  MOVE.W   I,D0       * Pick up the Value of I
+  MULU     #4,D0      * Eeach Address is a Longword
+  LEA      Table,A0   * A0 points to table 
+                      * of addresses
+  LEA      (A0,D0),A0 * A0 now points to case I 
+                      * in Table
+  MOVEA.L  (A0),A0    * A0 contains address of
+                      * case I handler
+  JMP      (A0)       * Executes case I handler
 *
 Table:
-  ORG      <address>    Table of exceptions
+  ORG      <address>  * Table of exceptions
 Action0:
-  DC.L     <address0>   Address of case 0 handler
+  DC.L     <address0> * Address of case 0 handler
 Action1:
-  DC.L     <address0>   Address of case 1 handler
+  DC.L     <address0> * Address of case 1 handler
 Action2:
-  DC.L     <address0>   Address of case 2 handler
+  DC.L     <address0> * Address of case 2 handler
 .
 .
 Action:    
-  NDC.L    <addressN>   Address of case N handler
+  NDC.L    <addressN> * Address of case N handler
 *
 EXCEPTION:        
-           ...          Exception handler here
+           ...        * Exception handler here
     
